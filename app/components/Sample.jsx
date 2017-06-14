ed4e457fcd324dcf09b29c600ddb1ce37d8a6cd5
Adhithya <adhithyaprabhu77@gmail.com>
2017-06-14 10:59:19 +0530

    initial commit in branch saample

---
+++ app/components/Sample.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+
+var Sample = React.createClass({
+	render(): function() {
+		return (
+
+			<div>
+			<p>hai sample component</p>
+			</div>
+
+			);
+	}
+});
+
+module.exports = Sample;
